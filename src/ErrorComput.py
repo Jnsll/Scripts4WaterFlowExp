3986861fb41f9a03f2d9734b64090f825b742a7e
Jnsll <june.benvegnu-sallou@irisa.fr>
2019-04-30 18:44:50 +0200

    feature : compute flood presence for each day of a simulation

--- src/ErrorComput.py
+++ src/ErrorComput.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+"""
+    Provides functions to compute error rate between the reference simulation and an alternative simulation.
+"""
+
 import os
 import numpy as np
 import numpy.testing as npt
@@ -14,6 +19,16 @@ import pandas as pd
 import threading
 import subprocess
 import math
+import flopy
+import pickle
+
+__author__ = "June Sallou"
+__maintainer__ = "June Sallou"
+__credits__ = ["June Sallou"]
+__license__ = "MIT"
+__version__ = "0.0.1"
+__date__ = "04/30/2019"
+__email__ = "june.benvegnu-sallou@univ-rennes1.fr"
 
 
 mainAppRepo = os.getcwd() + "/"
@@ -23,8 +38,7 @@ def getPathToSimulationDirectoryFromModelname(modelname, site_name):
     return os.path.join(mainAppRepo, site_name, modelname)
 
 def getSiteNameFromSiteNumber(site_number):
-    sites = pd.read_csv(mainAppRepo + "data/study_sites.txt", sep='\s+', header=0, index_col=0)
-    #site_number = 2
+    sites = pd.read_csv(mainAppRepo + r"data/study_sites.txt", sep='\s+', header=0, index_col=0)
     site_name = sites.index._data[site_number] + '/'
     return site_name
 
@@ -117,6 +131,78 @@ def computeErrorRatesFromModelnamesByInterpoliationOptiParalFixedInit(ref, model
 
 
 
+def computeFloodsFromModelnamesByInterpoliation(modelname, site_number,timestep=1):
+    site_name = getSiteNameFromSiteNumber(site_number)
+    repoSimu = getPathToSimulationDirectoryFromModelname(modelname, site_name)
+    
+    # Get values for topo
+    mf = flopy.modflow.Modflow.load(repoSimu + "/" + modelname + '.nam')
+    dis = flopy.modflow.ModflowDis.load(repoSimu + "/" + modelname + '.dis', mf)
+    topo = dis.top._array
+    
+    # Get heads values for simulation
+    simuHds = fpu.HeadFile(repoSimu + '/' + modelname + '.hds')
+    simuTimes = simuHds.get_times()
+    #simuKstpkper = simuHds.get_kstpkper()
+    
+    # Comput parameters
+    startTime = 0
+    endTime = 15340
+    criticalDepth = 0.3
+
+    #endTime+1 - startTime
+    floods = {}
+    #Pour chaque jour
+    for day in range(startTime, endTime+1):
+        print(day)
+
+        nbPeriod = 0
+        while (simuTimes[nbPeriod] < day+1) and (nbPeriod < len(simuTimes)):
+            nbPeriod+=1
+        print("nbPeriod : " + str(nbPeriod))
+        #On récupère la matrice de simulation alt supérieure
+
+        print(simuTimes[nbPeriod], day+1)
+        if math.isclose(simuTimes[nbPeriod], day+1, rel_tol=1e-3): #simuTimes[nbPeriod] == day+1
+            print("condition ==")
+            altHeadSup = simuHds.get_data(kstpkper=(timestep-1, nbPeriod))       
+            altHeadInf = altHeadSup
+            duree = int(simuTimes[nbPeriod])
+            pas = day
+        else :
+            altHeadSup = simuHds.get_data(kstpkper=(timestep-1, nbPeriod))        
+            altHeadInf = simuHds.get_data(kstpkper=(timestep-1, nbPeriod-1))
+            duree = int(simuTimes[nbPeriod] - simuTimes[nbPeriod-1])
+            pas = day - simuTimes[nbPeriod-1]
+
+        nbrowtot = altHeadInf.shape[1]        
+        nbcoltot = altHeadInf.shape[2]
+
+        onecol = [0]*nbcoltot
+        flood = [onecol]
+        for i in range(1, nbrowtot):
+            flood.append(onecol)
+
+        for nrow in range(nbrowtot):
+            for ncol in range(nbcoltot):
+                ss = getNonDryCellHdsValue(altHeadInf, nrow, ncol, altHeadInf.shape[0])
+                se = getNonDryCellHdsValue(altHeadSup, nrow, ncol, altHeadInf.shape[0])
+                ajoutSimu = (se - ss) / duree
+                s = ss + (ajoutSimu * pas)
+
+                d = topo[nrow][ncol]- s
+                if d > criticalDepth:
+                    flood[nrow][ncol]=1
+        
+        floods[day] = flood
+        
+    pickle.dump(floods, repoSimu + "/" + modelname + "_floods_pickle")
+        
+    return floods
+
+
+
+
 def storeErrorValuesIntoCSVFile(ref, modelname, site_name, periodNumber, simulatedDuration, mea, mre, rmse):
     simuRepo = getPathToSimulationDirectoryFromModelname(modelname, site_name)
     if (periodNumber == 0):
@@ -169,7 +255,9 @@ if __name__ == '__main__':
     parser.add_argument("-ts", "--timestep", type=int, required=False)
     parser.add_argument("-stime", "--starttime", type=int, required=False)
     parser.add_argument("-etime", "--endtime", type=int, required=False)
+    parser.add_argument("-flood", "--flood", action='store_true')
     #parser.add_argument("-c", "--corenumber", type=int, required=True)
+
     args = parser.parse_args()
 
     ref = args.ref
@@ -178,15 +266,19 @@ if __name__ == '__main__':
     timestep = args.timestep
     startTime = args.starttime
     endTime = args.endtime
+    flood = args.flood
     #coeur = args.corenumber
-
-    if ref is None:
-        ref = "model_time_0_geo_0_thick_1_K_86.4_Sy_0.1"
-
-    if timestep is None :
-        computeErrorRatesFromModelnamesByInterpoliationOptiParalFixedInit(ref, modelname, site_number, startTime, endTime)
+    
+    if flood:
+        computeFloodsFromModelnamesByInterpoliation(modelname, site_number,timestep=1)
     else :
-        computeErrorRatesFromModelnamesByInterpoliationOptiParalFixedInit(ref, modelname, site_number, startTime, endTime, timestep)
+        if (ref is None):
+            ref = "model_time_0_geo_0_thick_1_K_86.4_Sy_0.1"
+
+        if timestep is None :
+            computeErrorRatesFromModelnamesByInterpoliationOptiParalFixedInit(ref, modelname, site_number, startTime, endTime)
+        else :
+            computeErrorRatesFromModelnamesByInterpoliationOptiParalFixedInit(ref, modelname, site_number, startTime, endTime, timestep)
     # if timestep is None:
     #     timestep = 1
 
